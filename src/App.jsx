04a14b3ec7ce37511b9cc4eee5ed76f73d109233
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2023-12-03 15:21:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage';
+import Homepage from './pages/HomePage.jsx';
 import DetailCard from './pages/detailCard';
 
 function App() {
